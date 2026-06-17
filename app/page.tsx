dfc284371c1f806e4e40646ec7cd85e5a8050adb
farzaddm <farzad.d.m84@gmail.com>
2026-06-17 17:23:25 +0330

    feat: add charts

--- app/page.tsx
+++ app/page.tsx
@@ -1,12 +1,21 @@
+import AppBarChart from "@/components/AppBarChart";
+import AppAreaChart from "@/components/AppAreaChart";
+import AppPieChart from "@/components/AppPieChart";
 
 export default function Home() {
   return (
     <div className="grid gird-cols-1 md:grid-cols-2 2xl:grid-cols-4 gap-4">
-      <div className="bg-primary-foreground rounded-lg p-4 lg:col-span-2 xl:col-span-1 2xl:col-span-2">Test</div>
+      <div className="bg-primary-foreground rounded-lg p-4 lg:col-span-2 xl:col-span-1 2xl:col-span-2">
+        <AppBarChart/>
+      </div>
       <div className="bg-primary-foreground rounded-lg p-4">Test</div>
+      <div className="bg-primary-foreground rounded-lg p-4">
+        <AppPieChart/>
+      </div>
       <div className="bg-primary-foreground rounded-lg p-4">Test</div>
-      <div className="bg-primary-foreground rounded-lg p-4">Test</div>
-      <div className="bg-primary-foreground rounded-lg p-4 lg:col-span-2 xl:col-span-1 2xl:col-span-2">Test</div>
+      <div className="bg-primary-foreground rounded-lg p-4 lg:col-span-2 xl:col-span-1 2xl:col-span-2">
+        <AppAreaChart/>
+      </div>
       <div className="bg-primary-foreground rounded-lg p-4">Test</div>
     </div>
   );
---
+++ components/AppBarChart.tsx
@@ -0,0 +1,53 @@
+"use client"
+import {type ChartConfig, ChartContainer, ChartLegend, ChartLegendContent, ChartTooltip, ChartTooltipContent} from "@/components/ui/chart"
+import {Bar, BarChart, CartesianGrid, XAxis, YAxis} from "recharts";
+
+const chartConfig = {
+  desktop: {
+    label: "Desktop",
+    color: "var(--chart-1)",
+  },
+  mobile: {
+    label: "Mobile",
+    color: "var(--chart-4)",
+  },
+} satisfies ChartConfig
+
+
+const chartData = [
+  { month: "January", desktop: 186, mobile: 80 },
+  { month: "February", desktop: 305, mobile: 200 },
+  { month: "March", desktop: 237, mobile: 120 },
+  { month: "April", desktop: 73, mobile: 190 },
+  { month: "May", desktop: 209, mobile: 130 },
+  { month: "June", desktop: 214, mobile: 140 },
+];
+
+const AppBarChart = () => {
+  return (
+    <div>
+      <h2 className="font-medium text-lg mb-4">Total Revenue</h2>
+      <ChartContainer config={chartConfig}>
+        <BarChart data={chartData}>
+          <CartesianGrid vertical={false} />
+          <XAxis
+            dataKey="month"
+            tickLine={false}
+            tickMargin={10}
+            axisLine={false}
+            tickFormatter={(value) => value.slice(0, 3)}
+          />
+          <YAxis tickLine={false} tickMargin={10} axisLine={false} />
+          <ChartTooltip content={<ChartTooltipContent />} />
+          <ChartLegend content={<ChartLegendContent />} />
+          <Bar dataKey="desktop" fill="var(--color-desktop)" radius={4} />
+          <Bar dataKey="mobile" fill="var(--color-mobile)" radius={4} />
+
+        </BarChart>
+      </ChartContainer>
+    </div>
+  );
+};
+
+
+export default AppBarChart;
