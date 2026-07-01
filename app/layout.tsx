a50ffda664724d1ad20fc20d4890618f3b674ee3
farzaddm <farzad.d.m84@gmail.com>
2026-07-01 21:53:14 +0330

    feat: add readme

--- app/layout.tsx
+++ app/layout.tsx
@@ -28,7 +28,7 @@ export default async function RootLayout({children,}: Readonly<{ children: React
 
   return (
     <html lang="en" className={`${geistSans.variable} ${geistMono.variable} h-full antialiased`} suppressHydrationWarning>
-    <body className="min-h-full flex flex-col">
+    <body className="min-h-full flex flex-col" suppressHydrationWarning>
     <ThemeProvider
       attribute="class"
       defaultTheme="system"
